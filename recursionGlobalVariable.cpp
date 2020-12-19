aa1a05196e5d149e5e474e94ab2b02269831b478
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2020-12-19 19:00:21 +0530

    Create recursionGlobalVariable.cpp

---
+++ recursionGlobalVariable.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+using namespace std;
+// In this fuction the number n is add while function returning and not during the fucntion calling.
+int func(int n)
+{   static int x;
+    if(n>0)
+    {
+        x++;
+        return (func(n-1)+n);
+    }
+    return 0;
+}
+/*
+func(5)= (func(4)+5)+(func(3)+4)+(func(2)+3)+(func(1)+2)+(func(0)+1);
+*/
+int main()
+{
+    int n=5;
+    cout<<func(n);
+
+    return 0;
+}
