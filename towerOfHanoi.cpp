dfc6a1f141e797313448070cfc0aaded0bb3d8f2
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2020-12-23 20:28:16 +0530

    Update towerOfHanoi.cpp

--- towerOfHanoi.cpp
+++ towerOfHanoi.cpp
@@ -7,15 +7,16 @@ void towerOfHanoi(int num,string initial, string target,string auxilary)
 {
     if(num==1)
     {
-        cout<< "move disk 1 from tower" << initial << "to tower"<< target;
+        cout<< "move disk 1 from tower " << initial << " to tower "<< target<<endl;
         return;
     }
     towerOfHanoi(num-1,initial,auxilary,target);
-    cout<< "move disk" << num << "from tower" << initial << "to tower"<< target;
+    cout<< "move disk " << num << " from tower " << initial << " to tower "<< target<<endl;
     towerOfHanoi(num-1,auxilary,target,initial);
 }
 int main()
 {
+    towerOfHanoi(3,"A","B","C");
+
     return 0;
-    towerOfHanoi(4,"initial","target","auxilary");
 }
