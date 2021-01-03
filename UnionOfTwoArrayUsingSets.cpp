b21caa5a4fe082c7607dec7c32cd661bb2c77819
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2021-01-03 16:35:21 +0530

    Create UnionOfTwoArrayUsingSets.cpp

---
+++ UnionOfTwoArrayUsingSets.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        int size1, size2;
+        unordered_set <int> setUnion;
+        cin >> size1;
+        cin >> size2;
+        for (int i = 0; i < size1+size2 ; i++) {
+            int temp;
+            cin>> temp;
+             setUnion.insert(temp);
+        }
+    int res = setUnion.size();
+    cout<< res<< endl;
+    }
+    return 0;
+} 
