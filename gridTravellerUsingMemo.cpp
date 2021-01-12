d19375545e3157c697699a7e0a192e1ffcf666eb
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2021-01-12 19:04:17 +0530

    Update gridTravellerUsingMemo.cpp

--- gridTravellerUsingMemo.cpp
+++ gridTravellerUsingMemo.cpp
@@ -1,16 +1,32 @@
-include<iostream>
-
+#include<iostream>
+#include <string>
+#include<unordered_map>
 using namespace std;
 
-int findTotalWays(int rows, int columns)
-{
+unordered_map <string, int> memo;
 
-return count
+int findTotalMoves( int m, int n)
+{
+  if (n == 0 || m == 0)
+    return 0;
+  if (n == 1 && m == 1)
+    return 1;
+  string key = to_string (m) + "" + to_string (n);
+  unordered_map < string, int >::const_iterator got = memo.find (key);
+  if (got != memo.end ())
+    return memo[key];
+  memo[key] = findTotalMoves (m - 1, n);
+  memo[key] = findTotalMoves (m, n - 1);
+  return memo[key];
 }
 
-int main()
+int main ()
 {
+  int rows, coloumns;
+  cin >> rows >> coloumns;
 
-
-return 0;
+  cout << "total number of possible moves are " << findTotalMoves (rows,
+								   coloumns)
+    << endl;
+  return 0;
 }
