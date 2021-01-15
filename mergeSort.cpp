157e89e5beaf081affd540633cdd66c8ab50b12e
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2021-01-15 23:57:34 +0530

    Update mergeSort.cpp

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,47 +1,70 @@
 #include <iostream>
-#include <vector>
+ 
 using namespace std;
 
-vector<int> merge(int arr1[], int arr2[], int m, int n)
+void Merge(int a[], int low, int high, int mid)
 {
-    vector<int> result;
-    int i = 0, j = 0;
-    while (i<=m && j<=n)
-    {
-        if (arr1[i] > arr2[j])
-        {
-            result.push_back(arr2[j++]);
-        }
-        else
-        {
-            result.push_back(arr1[i++]);
-        }
-        
-    }
-    for (; i < m; i++)
-    {
-        result.push_back(arr1[i]);
-    }
-    for (; j < n; j++)
-    {
-        result.push_back(arr2[j]);
-    }
-    return (result);
+	int i, j, k, temp[high-low+1];
+	i = low;
+	k = 0;
+	j = mid + 1;
+ 
+	while (i <= mid && j <= high)
+	{
+		if (a[i] < a[j])
+		{
+			temp[k++] = a[i++];
+		}
+		else
+		{
+			temp[k] = a[j];
+			k++;
+			j++;
+		}
+	}
+ 
+	while (i <= mid)
+	{
+		temp[k] = a[i];
+		k++;
+		i++;
+	}
+ 
+	while (j <= high)
+	{
+		temp[k] = a[j];
+		k++;
+		j++;
+	}
+ 
+	for (i = low; i <= high; i++)
+	{
+		a[i] = temp[i-low];
+	}
 }
-void mergeSort(int arr[], int l, int r)
+
+void MergeSort(int a[], int low, int high)
 {
-
+	int mid;
+	if (low < high)
+	{
+		mid=(low+high)/2;
+		MergeSort(a, low, mid);
+		MergeSort(a, mid+1, high);
+		Merge(a, low, high, mid);
+	}
 }
-
+ 
 int main()
 {
-    int array1[5] = {0, 3, 5, 9, 17}, array2[3]={ 6, 8, 2};
-    int size = sizeof(array1)/sizeof(array1[0]);
-//mergeSort(arrat, 0, size - 1);
-vector <int> res = merge(array1,array2,5,3);
-
-for (int i =0; i< res.size(); i++)
-    cout<< res[i] << " ";
-return 0;
-
+	 int arr[5] = {0, 199, 5, 1, 17}, array2[3]={ 6, 8, 2};
+    int size = sizeof(arr)/sizeof(arr[0]);
+MergeSort(arr, 0, size - 1);
+//vector <int> res = merge(array1,array2,5,3);
+	// Printing the sorted data.
+	cout<<"\nSorted Data ";
+	for (int i = 0; i < size; i++)
+        cout<<arr[i]<<" ";
+ 
+	return 0;
 }
