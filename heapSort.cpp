ebdeddea817b100622c923a2d3ae37aad3bd074c
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2021-01-20 23:31:55 +0530

    Update heapSort.cpp

--- heapSort.cpp
+++ heapSort.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+void heapSort(int arr[], int size)
+{
+
+}
+
+int main()
+{
+int arr[10]={12,2,3,45,-10,23,0,9,23,77};
+int size= sizeof(arr)/sizeof(arr[0]);
+heapSort(arr,size);
+
+for(int i=0; i<size; i++)
+{
+  cout<< arr[i]<< " ";
+}
+  return 0;
+}
