ddb9f1b3805b720c37dddbe760acda1072b63b4c
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2021-02-08 12:18:17 +0530

    Update IterativeInorderTraversal.cpp

--- IterativeInorderTraversal.cpp
+++ IterativeInorderTraversal.cpp
@@ -1,1 +1,58 @@
+#include <iostream>
+#include <stack>
+#include <unordered_map>
 
+using namespace std;
+struct Node
+{
+    int value;
+    Node *leftNode;
+    Node *rightNode;
+
+    Node(int val)
+    {
+        this->value = val;
+        this->leftNode = this->rightNode = nullptr;
+    }
+};
+
+void iterativeInorder(Node *root)
+{
+
+    stack<Node *> iterativeStack;
+    if (root == nullptr)
+    {
+        cout << "tree is empty traversal is not possible" << endl;
+    }
+    while (true)
+    {
+        while (root != nullptr)
+        {
+            iterativeStack.push(root);
+            root = root->leftNode;
+        }
+        if (iterativeStack.empty())
+        {
+            break;
+        }
+        root = iterativeStack.top();
+        iterativeStack.pop();
+        cout << root->value << "  ";
+        root = root->rightNode;
+    }
+}
+
+int main()
+{
+
+    Node *root = new Node(12);
+    root->leftNode = new Node(2);
+    root->rightNode = new Node(3);
+    root->leftNode->leftNode = new Node(4);
+    root->leftNode->rightNode = new Node(5);
+    root->rightNode->rightNode = new Node(6);
+
+    iterativeInorder(root);
+
+    return 0;
+}
