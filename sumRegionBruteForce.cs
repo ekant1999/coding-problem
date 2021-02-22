cc3c849782af2b0eecb8461727768e7f9f1bf218
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2021-02-22 23:42:10 +0530

    Update sumRegionBruteForce.cs

--- sumRegionBruteForce.cs
+++ sumRegionBruteForce.cs
@@ -1,1 +1,30 @@
+using System;
+					
+public class SumRegion{
+  // time complexity-O(M*N) space-O(1)
+	public static int sumRegionBruteForce(int[,] array, int row1, int col1, int row2, int col2)
+	{	
+	int sum=0;
+		for(int i= row1; i<=row2; i++)
+		{
+			for(int j=col1; j<=col2; j++)
+			{
+				sum+= array[i,j];
+			}
+			
+		}
+		
+		return sum;
+	}
 
+
+};
+public class Program
+{
+	public static void Main()
+	{
+		int[,] matrix= {{3,0,1,4,2},{5,6,3,2,1},{1,2,0,1,5},{4,1,0,1,7},{1,0,3,0,5}};
+		int result= SumRegion.sumRegionBruteForce(matrix,2,1,4,3);
+		Console.WriteLine(result);
+	}
+}
