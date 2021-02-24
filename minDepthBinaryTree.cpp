bc2aca44ec35b72c4d61ba2e7488f74ec52f42f8
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2021-02-24 23:57:42 +0530

    Update minDepthBinaryTree.cpp

--- minDepthBinaryTree.cpp
+++ minDepthBinaryTree.cpp
@@ -1,6 +1,4 @@
-
 #include <queue> 
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -56,3 +54,29 @@ public:
      return minDepthHelper(root, DepthValuePair);   
     }
 };
+
+
+
+//solution without hashmap
+
+class Solution {
+public:
+    
+   int minDepth(TreeNode* root) {
+    if (root == NULL) return 0;
+    queue<TreeNode*> Q;
+    Q.push(root);
+    int i = 0;
+    while (!Q.empty()) {
+        i++;
+        int k = Q.size();
+        for (int j=0; j<k; j++) {
+            TreeNode* rt = Q.front();
+            if (rt->left) Q.push(rt->left);
+            if (rt->right) Q.push(rt->right);
+            Q.pop();
+            if (rt->left==NULL && rt->right==NULL) return i;
+        }
+    }
+    return -1;
+}};
