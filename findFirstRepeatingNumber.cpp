5026b99056dc09718d9b510046a8069611eeb7a0
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2021-03-05 00:19:38 +0530

    Update findFirstRepeatingNumber.cpp

--- findFirstRepeatingNumber.cpp
+++ findFirstRepeatingNumber.cpp
@@ -1,1 +1,18 @@
 
+#include <vector>
+using namespace std;
+/*
+array= {1,2,5,2,3,5,1}
+first repeating number = 2;
+*/
+int firstDuplicateValue(vector<int> array) { 
+	for(auto val : array)
+	{
+		int absVal= abs(val);
+		if(array[absVal-1]<0)
+			return absVal;
+		else
+			array[absVal-1] *=-1;
+	}
+	return -1; 
+}
