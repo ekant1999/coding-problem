96adb2effd5a013ef19d4730ee8f185ad6570a7a
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2021-03-07 00:49:29 +0530

    Update restoreIpAddresses.cpp

--- restoreIpAddresses.cpp
+++ restoreIpAddresses.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+  // time-O(1) space-O(1).
+    vector<string> restoreIpAddresses(string s) {
+        	vector<string> ret;
+        string ans;
+        for (int firstBLock=1; firstBLock<=3; firstBLock++)
+        for (int secondBLock=1; secondBLock<=3; secondBLock++)
+        for (int thirdBlock=1; thirdBlock<=3; thirdBlock++)
+        for (int fourthBlock=1; fourthBlock<=3; fourthBlock++)
+            if (firstBLock+secondBLock+thirdBlock+fourthBlock == s.length()) {
+                int A = stoi(s.substr(0, firstBLock));
+                int B = stoi(s.substr(firstBLock, secondBLock));
+                int C = stoi(s.substr(firstBLock+secondBLock, thirdBlock));
+                int D = stoi(s.substr(firstBLock+secondBLock+thirdBlock, fourthBlock));
+                if (A<=255 && B<=255 && C<=255 && D<=255)
+                    if ( (ans=to_string(A)+"."+to_string(B)+"."+to_string(C)+"."+to_string(D)).length() == s.length()+3)
+                        ret.push_back(ans);
+            }    
+        
+        return ret;
+    }
+};
