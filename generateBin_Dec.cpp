d7fddc7e26af2ed9c41c038a629b2d1f5513b81e
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2021-03-07 23:45:11 +0530

    Update and rename genrateBin_Dec.cpp to generateBin_Dec.cpp

--- generateBin_Dec.cpp
+++ generateBin_Dec.cpp
@@ -3,35 +3,35 @@
 #include <vector>
 #include<string>
 using namespace std;
-void genrateBin(int num, std::vector<string> &output, string res)
+void generateBin(int num, std::vector<string> &output, string res)
 {
     if(num==0){
         output.push_back(res);
         return;
     }
-    genrateBin(num-1,output,res+'0');
-    genrateBin(num-1,output,res+'1');
+    generateBin(num-1,output,res+'0');
+    generateBin(num-1,output,res+'1');
 }
-void genrateDec(int num, std::vector<string> &output, string res)
+void generateDec(int num, std::vector<string> &output, string res)
 {
     if(num==0){
         output.push_back(res);
         return;
     }
     for(int i=0; i<10; i++)
-    genrateDec(num-1,output,res+to_string(i));
+    generateDec(num-1,output,res+to_string(i));
 }
 int main()
 {
     int num=5;
     vector<string> res;
-    genrateBin(num,res,"");
+    generateBin(num,res,"");
     for(auto i : res)
     {
         std::cout << i << std::endl;
     }
     vector<string> res1;
-    genrateDec(5,res1,"");
+    generateDec(5,res1,"");
     for(auto i : res1)
     {
         std::cout << i << std::endl;
