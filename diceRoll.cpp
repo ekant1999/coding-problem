de1d93fce2a2895cc9c987dc334c48b09864b752
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2021-03-11 00:14:30 +0530

    Update diceRoll.cpp

--- diceRoll.cpp
+++ diceRoll.cpp
@@ -1,3 +1,56 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Using the backtracking we are creating all type of sets we are chosing one case exploring it and then unchosing it and then
+//re-exploring the second case and we do this until entire pattern is genrated or entire permutation of the set is created.
+
+void generateDiceRoll(int num, std::vector<vector<int> >& output,
+    vector<int> result)
+{
+    if (num == 0) {
+        output.push_back(result);
+        return;
+    }
+    for (int i = 1; i < 7; i++) {
+        result.push_back(i);
+        generateDiceRoll(num - 1, output, result);
+        result.pop_back();
+    }
+}
+int main()
+{
+    std::vector<vector<int> > res;
+    generateDiceRoll(2, res, {});
+    for (auto i : res) {
+        for (auto j : i)
+            std::cout << j << ' ';
+        std::cout << std::endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
