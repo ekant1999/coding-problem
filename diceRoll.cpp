d2d6ad02957de7ca2950d997ded3c08b1f0b02d8
EKANT KHUSHAL KAPGATE <2016bcs105@sggs.ac.in>
2021-03-10 23:30:16 +0530

    Update diceRoll.cpp

--- diceRoll.cpp
+++ diceRoll.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+void generateDiceRoll(int num, std::vector<string> &output, string result)
+{
+    if(num==0){
+        output.push_back(result);
+        return;
+    }
+    for(int i=1; i<7; i++){
+    if(num>1)
+    generateDiceRoll(num-1,output,result + to_string(i) +", ");
+    else
+    generateDiceRoll(num-1,output,result + to_string(i));
+    
+    }
+}
+int main()
+{
+    std::vector<string> res;
+    generateDiceRoll(2,res,"");
+    for(auto i: res)
+    std::cout << i << std::endl;
+    return 0;
+}
